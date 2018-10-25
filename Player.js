a7f30a315eeed6d964dbc4d60651af54a3185c6b
Peter Remenyi <Peter.Remenyi@logmein.com>
2018-10-25 15:45:46 +0200

    fix card

--- Player.js
+++ Player.js
@@ -29,7 +29,7 @@ class Player {
         let hand = "";
 
         let card1 = holeCards[0];
-        let card2 = holeCards[2];
+        let card2 = holeCards[1];
 
         if ((card1.rank == "10" || card1.rank == "J" ||  card1.rank == "Q" ||  card1.rank == "K" ||  card1.rank == "A")&&(card2.rank == "10" || card2.rank == "J" ||  card2.rank == "Q" ||  card2.rank == "K" ||  card2.rank == "A")){
             betValue = 1000;
