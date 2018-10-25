7b1852c3bcc36cd47840a6d3eae6fc2d5dc75724
szbotond <szbotond@logmein.com>
2018-10-25 12:34:37 +0200

    remove code

--- src/JSON.cs
+++ src/JSON.cs
@@ -2,15 +2,15 @@ namespace PokerBot
 {
     internal class JSON
     {
-        public static GameState DeserializeJSONGameState(string serializedResponse)
-        {
-            if (serializedResponse == null) throw new ArgumentNullException(nameof(serializedResponse));
+        // public static GameState DeserializeJSONGameState(string serializedResponse)
+        // {
+        //     if (serializedResponse == null) throw new ArgumentNullException(nameof(serializedResponse));
 
-            DataContractJsonSerializer serializer = new DataContractJsonSerializer(typeof(GameState));
-            MemoryStream memoryStream = new MemoryStream(Encoding.ASCII.GetBytes(serializedResponse));
-            TResponse response = (GameState)serializer.ReadObject(memoryStream);
+        //     DataContractJsonSerializer serializer = new DataContractJsonSerializer(typeof(GameState));
+        //     MemoryStream memoryStream = new MemoryStream(Encoding.ASCII.GetBytes(serializedResponse));
+        //     TResponse response = (GameState)serializer.ReadObject(memoryStream);
 
-            return response;
-        }
+        //     return response;
+        // }
     }
 }
