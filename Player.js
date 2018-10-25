a8f0d7eaaa904dc1dce210b40697bead263da85a
Peter Remenyi <Peter.Remenyi@logmein.com>
2018-10-25 15:45:19 +0200

    fix log

--- Player.js
+++ Player.js
@@ -39,7 +39,7 @@ class Player {
           hand += card.rank + card.suit.substring(0,1) + comma;
         }  
     
-        console.log(bet);
+        console.log(betValue);
         console.log(community);
         console.log(hand);
     
